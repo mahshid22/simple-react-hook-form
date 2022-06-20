097e07c7e7dc997711711ef62caf17b9c596752f
mahshid22 <mahshid.pouyafar@gmail.com>
2022-06-20 21:41:08 +0430

    remove form handling from form context because we add react hook form to project

--- context/formContex.jsx
+++ context/formContex.jsx
@@ -1,27 +1,11 @@
 import { useState, createContext } from "react";
 
-const initialState = {
-  name: "",
-  mobile: "",
-  description: "",
-};
-const FormContext = createContext({
-  form: initialState,
-});
+const FormContext = createContext({});
 const FormProvider = ({ children }) => {
-  const [form, setForm] = useState(initialState);
   const [loading, setLoading] = useState(false);
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
+
   return (
-    <FormContext.Provider
-      value={{ form, handleInputChange, loading, setLoading }}
-    >
+    <FormContext.Provider value={{ loading, setLoading }}>
       {children}
     </FormContext.Provider>
   );
